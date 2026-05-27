8210c6f2e0cf5e041239fde0db9388b14716f657
md-zeon <zeon.cse@gmail.com>
2026-05-28 00:38:24 +0600

    feat: enhance NavbarContent with account and cart details, and improve layout

--- src/app/(public)/_components/navigation/NavbarContent.tsx
+++ src/app/(public)/_components/navigation/NavbarContent.tsx
@@ -1,26 +1,56 @@
+import { ShoppingCart, Truck, UserCircle } from "lucide-react";
 import { NavbarSearch } from "./NavbarSearch";
 
 const NavbarContent = () => {
   return (
-    <nav className="sticky top-0 z-50 w-full backdrop-blur bg-white/75 border-b">
+    <nav className="sticky top-0 z-50 w-full backdrop-blur">
       <div className="container mx-auto flex h-16 items-center justify-between gap-4 px-4 md:px-6">
-        {/* 1. Logo */}
+        {/* Logo */}
         <div className="flex items-center gap-1 cursor-pointer shrink-0">
           <span className="text-4xl font-black tracking-tight">Oshudpati</span>
         </div>
 
-        {/* 2. Search Area  */}
+        {/* Search Area  */}
         <div className="flex-1 max-w-xl mx-2 md:mx-8">
           <NavbarSearch />
         </div>
 
-        {/* 3. Navigation */}
+        {/* Navigation */}
         <div className="flex items-center gap-2 md:gap-4 shrink-0">
           {/* Track Your Order */}
+          <div className="flex items-center gap-1 cursor-pointer">
+            <Truck className="text-slate-600 inline-block" size={36} />
+            <div className="font-medium leading-tight">
+              <p className="text-sm leading-tight font-medium text-slate-600 hover:text-blue-600 cursor-pointer">
+                Track
+              </p>
+              <span className="text-base leading-tight">Your Order</span>
+            </div>
+          </div>
 
           {/* Account */}
+          <div className="flex items-center gap-1 cursor-pointer">
+            <UserCircle className="text-slate-600 inline-block" size={36} />
+            <div className="font-medium leading-tight">
+              <p className="text-sm leading-tight font-medium text-slate-600 hover:text-blue-600 cursor-pointer">
+                Hello, Sign in
+              </p>
+              <span className="text-base leading-tight">Account</span>
+            </div>
+          </div>
 
           {/* Cart */}
+          <div className="flex items-center gap-1 cursor-pointer">
+            <div className="rounded-full border-2 p-1">
+              <ShoppingCart className="text-slate-600 inline-block" size={24} />
+            </div>
+            <div className="font-medium leading-tight">
+              <p className="text-sm leading-tight font-medium text-slate-600 hover:text-blue-600 cursor-pointer">
+                Cart
+              </p>
+              <span className="text-base leading-tight">0 items</span>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
