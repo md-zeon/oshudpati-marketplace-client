0c12ebdea9c9d39b25ffb27a9e6e8e18202991d9
md-zeon <zeon.cse@gmail.com>
2026-05-28 10:34:10 +0600

    added NotFoundPage component with Empty state UI

---
+++ src/app/not-found.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import { HomeIcon } from "lucide-react";
+import Link from "next/link";
+import NavbarContent from "./(public)/_components/navigation/NavbarContent";
+
+export const metadata = {
+  title: "Page Not Found",
+  description: "The page you are looking for does not exist.",
+};
+
+const NotFoundPage = () => {
+  return (
+    <div className="relative min-h-screen w-full items-center justify-center overflow-hidden">
+      <NavbarContent />
+      <Empty>
+        <EmptyHeader>
+          <EmptyTitle className="mask-b-from-20% mask-b-to-80% font-extrabold text-[15rem] text-transparent bg-linear-to-r from-foreground/20 via-foreground to-foreground/20 bg-clip-text">
+            404
+          </EmptyTitle>
+          <EmptyDescription className="-mt-8 text-nowrap text-foreground/80 text-4xl font-semibold">
+            That Page Cant Be Found
+          </EmptyDescription>
+          <p>
+            It looks like nothing was found at this location. <br /> Maybe try
+            to search for what you are looking for?
+          </p>
+        </EmptyHeader>
+        <EmptyContent>
+          <Button asChild variant="outline">
+            <Link href="/">
+              <HomeIcon data-icon="inline-start" /> Go to Home
+            </Link>
+          </Button>
+        </EmptyContent>
+      </Empty>
+    </div>
+  );
+};
+
+export default NotFoundPage;
