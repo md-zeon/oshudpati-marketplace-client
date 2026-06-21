da75472234bb1a7706346f90fe79b8c198fe2c5f
md-zeon <zeon.cse@gmail.com>
2026-06-21 16:45:28 +0600

    Followed Deployment Guide

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,8 @@
-import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   // The base URL of the authentication server
-  baseURL: env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: typeof window !== "undefined" ? window.location.origin : "",
+  fetchOptions: {
+    credentials: "include",
+  },
 });
